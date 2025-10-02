df3400c85249b97828cbb65aaeccd332bf595637
Twinkywinkie <quincy.kapenga@kwinsie.com>
2025-10-02 09:45:01 +0200

    SLAYYYYY

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 // JavaScript Document
 console.log("hi");
+
+let hamburgerButton = document.querySelector("header button");
+let nav = document.querySelector("header nav");
+
+hamburgerButton.addEventListener("click", toggleButton);
+
+function toggleButton (){
+  hamburgerButton.classList.toggle("is-open")
+
+  nav.classList.toggle("is-visible");
+}
+
+const isExpanded = hamburgerButton.classList.contains("is-open");
+hamburgerButton.setAttribute("aria-expanded", isExpanded);
